6a8550dc1ecd3c349618420cd28d906ba68ee852
Leandro Soto <soto.lean@gmail.com>
2024-07-12 17:12:35 -0300

    Se agrego el error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterDetailComponent } from './Components/character-detail/character-detail.component';
 import { CharacterListComponent } from './Components/character-list/character-list.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { Error404Component } from './Pages/error404/error404.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/characters', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'characters', component: CharacterListComponent },
-  { path: 'characters/:id', component: CharacterDetailComponent }
+  { path: 'characters/:id', component: CharacterDetailComponent},
+  { path: '404', component: Error404Component},
+  { path: '**', redirectTo:'/404'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CharacterListComponent } from './Components/character-list/character-list.component';
 import { CharacterDetailComponent } from './Components/character-detail/character-detail.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { Error404Component } from './Pages/error404/error404.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CharacterListComponent,
-    CharacterDetailComponent
+    CharacterDetailComponent,
+    HomeComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
